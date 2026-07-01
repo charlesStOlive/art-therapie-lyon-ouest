a31d31232e6e37bbc7c0f0693d472a23a305fe3b
charles saintolive <charles.stolive@gmail.com>
2026-07-01 08:26:19 +0200

    Correction titre et description

--- resources/views/components/filament-static-pages/blocks/shared/description.blade.php
+++ resources/views/components/filament-static-pages/blocks/shared/description.blade.php
@@ -5,8 +5,6 @@
 
 @if ($description)
     <div class="text-center {{ $class }}">
-        <div class="text-lg md:text-xl text-gray-600 mx-auto max-w-4xl whitespace-break-spaces">
-            {{ $description }}
-        </div>
+        <div class="text-lg md:text-xl text-gray-600 mx-auto max-w-4xl whitespace-break-spaces">{{ $description }}</div>
     </div>
 @endif
--- resources/views/components/filament-static-pages/blocks/shared/title.blade.php
+++ resources/views/components/filament-static-pages/blocks/shared/title.blade.php
@@ -14,8 +14,7 @@
             @if ($couleurPrimaire === 'primary-brush') prose-brush prose-brush-primary
             @elseif ($couleurPrimaire === 'secondary-brush') prose-brush prose-brush-secondary
             @elseif ($couleurPrimaire === 'primary') prose-strong:text-primary-500
-            @elseif ($couleurPrimaire === 'secondary') prose-strong:text-secondary-500 @endif
-        ">
+            @elseif ($couleurPrimaire === 'secondary') prose-strong:text-secondary-500 @endif">
             {!! $title !!}
         </div>
     </div>
